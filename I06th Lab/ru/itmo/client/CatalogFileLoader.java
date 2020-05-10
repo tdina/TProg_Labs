b71d78ac273613b93875d2a3199ca6e85224897b
tdina <61597145+tdina@users.noreply.github.com>
2020-05-10 17:38:26 +0300

    Update CatalogFileLoader.java
    Add closing a file

--- I06th Lab/ru/itmo/client/CatalogFileLoader.java
+++ I06th Lab/ru/itmo/client/CatalogFileLoader.java
@@ -9,6 +9,7 @@ import ru.itmo.exceptions.ItemAlreadyExistsException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -19,32 +20,25 @@ class CatalogFileLoader implements CatalogLoader {
         this.fileName = fileName;
     }
 
-    public void load(ItemCatalog cat) throws CatalogLoadException {
+    public void load(ItemCatalog cat) throws CatalogLoadException, IOException {
         File f = new File(fileName);
-        FileInputStream fis;
         String line;
-        try {
-            fis = new FileInputStream(f);
+        try (FileInputStream fis = new FileInputStream(f)) {
             Scanner s = new Scanner(fis);
 
             while(s.hasNextLine()){
                 line = s.nextLine();
-                if(line.length()==0) break;
+                if(line.length() == 0) break;
                 String[] item_fld = line.split(";");
                 String name = item_fld[0];
                 float price = Float.parseFloat(item_fld[1]);
                 short expires = Short.parseShort(item_fld[2]);
-                FOODItem item = new FOODItem(name,price,null, new Date(),expires);
+                FOODItem item = new FOODItem(name, price, null, new Date(), expires);
                 cat.addItem(item);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new CatalogLoadException();
-        } catch (ItemAlreadyExistsException e) {
+        } catch (FileNotFoundException | ItemAlreadyExistsException e) {
             e.printStackTrace();
             throw new CatalogLoadException();
         }
+
     }
-
-
-}
